07ea7563d9d677b341826754710b3dc1127eda62
hasanarofid <hasanarofid@gmail.com>
2023-10-18 10:14:04 +0700

    webview contoh

--- src/webView.js
+++ src/webView.js
@@ -1,13 +1,25 @@
 import { WebView } from 'react-native-webview';
 import React from 'react'
 
+// export default function webView() {
+//   return (
+//     <WebView
+//     source={{
+//       uri: 'https://rsudrsoetomo.jatimprov.go.id/'
+//     }}
+//     style={{ marginTop: 20 }}
+//   />
+//   )
+// }
+
 export default function webView() {
-  return (
-    <WebView
-    source={{
-      uri: 'https://rsudrsoetomo.jatimprov.go.id/'
-    }}
-    style={{ marginTop: 20 }}
-  />
-  )
-}
+    return (
+      <WebView
+      originWhitelist={['*']}
+      source={{
+        html: '<h1><center>CONTOH WEBVIEW HTML</center></h1>'
+      }}
+     />
+    )
+  }
+  
